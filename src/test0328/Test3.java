275d02e4ea1667a1c106a5e1b4946f3ffa8cd6d3
GD <GD@gdj>
2024-03-28 16:16:39 +0900

    20240328-1616

--- src/test0328/Test3.java
+++ src/test0328/Test3.java
@@ -5,8 +5,8 @@ package test0328;
  *   생성자 : 멤버변수를 매개변수로 입력받아 초기화
  *   멤버메서드 : toString을 결과에 맞도록 오버라이딩하기
  * EastCardDeck 클래스 구현하기
- *  멤버변수 : cards : SutdaCard 20장을 저장할 배열
- *  생성자 : SutdaCard 20장을 생성하여 cards 배열에 저장
+ *  멤버변수 : cards : EastCard 20장을 저장할 배열
+ *  생성자 : EastCard 20장을 생성하여 cards 배열에 저장
  *  메서드 : toString() 오버라이딩하여 결과가 출력되도록 구현
   [결과]    
   1K,2,3K,4,5,6,7,8K,9,10,1,2,3,4,5,6,7,8,9,10,
